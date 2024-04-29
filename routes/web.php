73f807035938d2dabd74704b24672be395cd21fc
Jhonny Pirela <jhosagid7@gmail.com>
2024-04-29 12:29:51 -0400

    Create livewire Componet chat-list

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Chat\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::get('/chat', Index::class)->name('chat.index');
